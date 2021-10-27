49c400a4f8b2250aa2be5a379f4fe169192311af
Guido Michel Oudin <guidooudin1@gmail.com>
2021-10-27 15:08:00 -0300

    Agregada la funcion de hacer una nueva tarea y obtener la lista de tareas

--- scripts/tasks2.js
+++ scripts/tasks2.js
@@ -2,9 +2,55 @@ window.addEventListener('load', () => {
     /* logica de nuestra app */
     const apiBaseUrl = 'https://ctd-todo-api.herokuapp.com/v1'
     const jwt = localStorage.getItem('token');
-    const nodoNombreUsuario = this.document.querySelector('.user-info p')
-
+    const nodoNombreUsuario = this.document.querySelector('.user-info p');
+    const formulario = document.querySelector('.nueva-tarea');
+    const inputNuevaTarea = this.document.querySelector('.nueva-tarea input');
     
+    /* Obtengo la lista comleta de tareas */
+    obtenerListaTareas(`${apiBaseUrl}/tasks`, jwt);
+    function obtenerListaTareas(url, token) {
+            const settings = { method: 'GET', 
+            headers: { authorization: token } };
+            
+            fetch(url,settings)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+        })
+    };
+    /* Crear nuevas tareas */
+    
+    formulario.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        const nuevaTarea = {
+            "description": inputNuevaTarea.value,
+            "completed": false
+        }
+        
+        console.log(inputNuevaTarea.value);
+        crearTarea(`${apiBaseUrl}/tasks`, jwt, nuevaTarea);
+        formulario.reset();
+
+    })
+    function crearTarea(url, token, payload) {
+        const settings = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                authorization : token
+            },
+            body: JSON.stringify(payload)
+        };
+        
+            fetch(url,settings)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+        })
+        }
+    
+    /* Obtengo los datos del usuario */
     obtenerDatosUsuario(`${apiBaseUrl}/users/getMe`, jwt)
     function obtenerDatosUsuario(url, token) {
         const settings = { method: 'GET', 
@@ -16,7 +62,7 @@ window.addEventListener('load', () => {
             console.log(data);
             nodoNombreUsuario.innerText = data.firstName + " " + data.lastName;
     })
-
-
+    
+    
     }
 });
